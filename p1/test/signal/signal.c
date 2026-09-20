d6da970fb65aaa735176fa9dc5269a7591ed92dc
aleandreuc <aleandreuc@gmail.com>
2026-09-20 11:20:26 +0200

    signal

---
+++ p1/test/signal/signal.c
@@ -0,0 +1,23 @@
+#include <signal.h>
+// Para evitar warnings con alarm() y signal():
+#include <stdio.h>
+#include <unistd.h>
+
+void alarma() {
+    printf("acabo de recibir un SIGALRM\n");
+}
+
+int main() {
+    signal(SIGALRM,alarma);
+    printf("acabo de programar la captura de un SIGALRM\n");
+    
+    alarm(3);
+    printf("Ahora he programado la alarma en 3 seg.\n");
+    pause();
+    
+    printf("vuelvo a programar la alarma\n");
+    alarm(3);
+    pause();
+    
+    printf("En POSIX esta línea nunca se ejecutaría porque me ha matado el SIGALRM\n");
+}
